3fd0d215ba7473d8bc54a91483ca97e1096c847d
khugar <ksangon98@gmail.com>
2019-03-20 11:33:40 +0100

    Exercici C Acabat

--- app/src/main/java/com/example/homersimpson_kevinsanchezgonzalez/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_kevinsanchezgonzalez/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.homersimpson_kevinsanchezgonzalez;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean visible;
+    ImageView Blau, Verd, Vermell, Ull, Donut;
     AnimationDrawable simpsonsTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,40 @@ public class MainActivity extends AppCompatActivity {
         ImageView titol =(ImageView)findViewById(R.id.theSimpsonsTitle);
         titol.setBackgroundResource(R.drawable.title_animation);
         simpsonsTitle = (AnimationDrawable) titol.getBackground();
+        final ImageView Blau = (ImageView)findViewById(R.id.ivBlau);
+        final ImageView Vermell = (ImageView)findViewById(R.id.ivVermell);
+        final ImageView Verd = (ImageView)findViewById(R.id.ivVerd);
+        final ImageView Ull = (ImageView)findViewById(R.id.ivUll);
+        final ImageView Donut = (ImageView)findViewById(R.id.ivDonut);
+
+        Blau.setVisibility(View.INVISIBLE);
+        Vermell.setVisibility(View.INVISIBLE);
+        Verd.setVisibility(View.INVISIBLE);
+        Ull.setVisibility(View.INVISIBLE);
+        Donut.setVisibility(View.INVISIBLE);
+        visible = false;
+
+        titol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(visible==false){
+                Blau.setVisibility(View.VISIBLE);
+                Verd.setVisibility(View.VISIBLE);
+                Vermell.setVisibility(View.VISIBLE);
+                Ull.setVisibility(View.VISIBLE);
+                Donut.setVisibility(View.VISIBLE);
+                visible=true;}
+                else{
+                    Blau.setVisibility(View.INVISIBLE);
+                    Vermell.setVisibility(View.INVISIBLE);
+                    Verd.setVisibility(View.INVISIBLE);
+                    Ull.setVisibility(View.INVISIBLE);
+                    Donut.setVisibility(View.INVISIBLE);
+                    visible=false;
+
+                }
+                }
+        });
     }
 
     @Override
@@ -22,4 +61,5 @@ public class MainActivity extends AppCompatActivity {
         super.onWindowFocusChanged(hasFocus);
         simpsonsTitle.start();
     }
+
 }

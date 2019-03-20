e32f615f5c805f685f6181e0edd6aef4c0f52de2
khugar <ksangon98@gmail.com>
2019-03-20 11:01:36 +0100

    Exercici B Acabat

--- app/src/main/java/com/example/homersimpson_kevinsanchezgonzalez/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_kevinsanchezgonzalez/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.homersimpson_kevinsanchezgonzalez;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable simpsonsTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView titol =(ImageView)findViewById(R.id.theSimpsonsTitle);
+        titol.setBackgroundResource(R.drawable.title_animation);
+        simpsonsTitle = (AnimationDrawable) titol.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        simpsonsTitle.start();
     }
 }
